c8e228247612f17ea2f92b28a737be386a2624f7
helios-22 <64339349+helios-22@users.noreply.github.com>
2020-05-01 22:05:02 +0100

    Update weapon_srp_659.lua

--- weapon/weapon_srp_659.lua
+++ weapon/weapon_srp_659.lua
@@ -1,5 +1,5 @@
 ITEM.Name = "S&W 659"
-ITEM.Desc = "A chrome (shine not included) pistol for the stalker more concerned with bling. Still a decent pistol but just a pistol.  Uses 9x19."
+ITEM.Desc = "A chrome (shine not included) pistol for the stalker more concerned with bling. Uses 9x19."
 ITEM.Model = "models/weapons/tfa_ins2/w_sw659.mdl"
 ITEM.WeaponClass = "tfa_ins2_sw659"
 ITEM.Weight = 1

03ddcc7b4c0b03e5e2663ba20a05495b93db421e
scunky11 <39074083+scunky11@users.noreply.github.com>
2020-05-06 18:22:47 -0400

    Update weapon_srp_cz75.lua

--- weapon/weapon_srp_cz75.lua
+++ weapon/weapon_srp_cz75.lua
@@ -12,5 +12,5 @@ ITEM.CamPos 		= Vector( 0, 100, 25 );
 ITEM.LookAt 		= Vector( 1, 0, 0 );
 ITEM.SelfRepairCondition = 75
 ITEM.AmmoType = "9x19"
-ITEM.License = LICENSE_BLACK;
+ITEM.License = "A"
 ITEM.BulkPrice = 45000

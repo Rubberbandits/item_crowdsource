f61da8e68723dbce97732703faa3c40abce46558
scunky11 <39074083+scunky11@users.noreply.github.com>
2020-05-06 18:35:17 -0400

    Update weapon_srp_fort500.lua

--- weapon/weapon_srp_fort500.lua
+++ weapon/weapon_srp_fort500.lua
@@ -12,6 +12,6 @@ ITEM.CamPos 		= Vector( 100, 100, 25 )
 ITEM.LookAt 		= Vector( 14, 0, 0 )
 ITEM.SelfRepairCondition = 60
 ITEM.AmmoType = "9x19"
-ITEM.License = LICENSE_BLACK;
+ITEM.License = "A";
 ITEM.BulkPrice = 55000
 ITEM.Slot = 2

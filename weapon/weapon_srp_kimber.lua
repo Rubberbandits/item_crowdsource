cfb0c4a4d5a3ace60b2dc08459eb5302831841ca
scunky11 <39074083+scunky11@users.noreply.github.com>
2020-05-06 18:45:43 -0400

    Update weapon_srp_kimber.lua

--- weapon/weapon_srp_kimber.lua
+++ weapon/weapon_srp_kimber.lua
@@ -12,5 +12,5 @@ ITEM.CamPos 		= Vector( 0, 100, 25 );
 ITEM.LookAt 		= Vector( 1, 0, 0 );
 ITEM.SelfRepairCondition = 90
 ITEM.AmmoType = "45acp"
-ITEM.License = LICENSE_BLACK;
+ITEM.License = "C"
 ITEM.BulkPrice = 105000

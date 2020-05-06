03c2e13fa49c964bfe53e629f6f7ee837f36965a
scunky11 <39074083+scunky11@users.noreply.github.com>
2020-05-06 18:37:22 -0400

    Update weapon_srp_glock.lua

--- weapon/weapon_srp_glock.lua
+++ weapon/weapon_srp_glock.lua
@@ -12,6 +12,6 @@ ITEM.CamPos 		= Vector( 0, 100, 25 )
 ITEM.LookAt 		= Vector( 1, 0, 0 )
 ITEM.SelfRepairCondition = 75
 ITEM.AmmoType = "9x19"
-ITEM.License = LICENSE_BLACK;
+ITEM.License = "B"
 ITEM.BulkPrice = 65000
 ITEM.Slot = 2
